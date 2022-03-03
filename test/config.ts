55d50e5ed3f86feccff9e51f0a55dfbabb968ec3
Adwithya Magow <amagow@finverse.com>
2022-03-03 15:08:17 +0800

    Add some config details

---
+++ test/config.ts
@@ -0,0 +1,8 @@
+// These are being hardcoded for the test.
+// Obtain these from https://dashboard.finverse.com
+export const config = {
+  apiHost: 'https://demo-api.dev.finverse.net',
+  redirectURI: 'https://demo-api.dev.finverse.net/callback',
+  clientId: 'clientId',
+  clientSecret: 'clientSecret',
+};
