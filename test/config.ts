9627636d99f143b08b1a222981e45993031bc660
Adwithya Magow <amagow@finverse.com>
2022-03-03 15:14:25 +0800

    Update config urls

--- test/config.ts
+++ test/config.ts
@@ -1,8 +1,8 @@
 // These are being hardcoded for the test.
 // Obtain these from https://dashboard.finverse.com
 export const config = {
-  apiHost: 'https://demo-api.dev.finverse.net',
-  redirectURI: 'https://demo-api.dev.finverse.net/callback',
+  apiHost: 'https://example.com',
+  redirectURI: 'https://example.com/callback',
   clientId: 'clientId',
   clientSecret: 'clientSecret',
 };
