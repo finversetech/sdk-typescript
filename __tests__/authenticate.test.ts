7f7d2582175ab188c3eaf3612ddad976bc82f5f5
Adwithya Magow <amagow@finverse.com>
2022-03-04 14:35:46 +0800

    Add authenticate test

---
+++ __tests__/authenticate.test.ts
@@ -0,0 +1,33 @@
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+
+import { config, grantType } from './config';
+import { Configuration, PublicApi } from '..';
+
+let mock = new MockAdapter(axios);
+
+it('Get customer access token', async () => {
+  // Variables
+  const access_token = 'customer-token';
+  const response = { access_token };
+  const url = `${config.apiHost}/auth/customer/token`;
+  const requestBody = {
+    client_id: config.clientId,
+    client_secret: config.clientSecret,
+    grant_type: grantType.CLIENT_CREDENTIALS,
+  };
+
+  // Mocking
+  mock.onPost(url, requestBody).reply(200, response);
+
+  // Make Request
+  const configuration = new Configuration({ basePath: config.apiHost });
+  const got = await new PublicApi(configuration).generateCustomerAccessToken({
+    client_id: config.clientId,
+    client_secret: config.clientSecret,
+    grant_type: grantType.CLIENT_CREDENTIALS,
+  });
+
+  // Expect
+  expect(got.data.access_token).toBe(access_token);
+});
